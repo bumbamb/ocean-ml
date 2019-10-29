4875db6c4988055abf62d7dd2b4dcca8e7e7fefb
Isabel <isabel103003@gmail.com>
2019-10-29 17:56:32 -0400

    nearly commited challenges

--- session-10-29/session-10-29-review.py
+++ session-10-29/session-10-29-review.py
@@ -2,22 +2,26 @@
 
 # Reviewing While loops:
 # Write code to print multiples of 5 from 45 to 90
-
+#i =45
+#while i <91:
+	#print(i)
+	#i += 5
 
 # With your partner: discuss: What is a set? What is it good for?
 # Write your response as a comment here:
-#
+#an ordered group of things in python and there can be no duplicates
 
 # Create a variable called "myset" and put whatever you want in it:
+myset= {1,2,3,4,5}
 
 
-
-# Create a 2D numpy array of size 3x5 with numbers 1-5:
-
-
+# Create a 2D numpy array of size 3x5 with numbers 1-15:
+import numpy as np
+a = np.arange(1,16).reshape(3,5)
+print(a)
 
 # From your array, print the number 7:
-
+print(a[1,1])
 
 
 # Challenge: use while loop to print the following pattern:
@@ -25,6 +29,8 @@
 # ***
 # **
 # *
-
-
-
+b= 0
+star=("*"+"*"+"*"+"*")
+while b<4:
+	print(star- "*")
+	b+=1
