435a8168b3e2179f7b7a2f97f7de796f71b9d1c9
Isabel <isabel103003@gmail.com>
2019-12-17 17:01:59 -0500

    12-17

--- session-12-10/while_loops_practice.py
+++ session-12-10/while_loops_practice.py
@@ -1,7 +1,28 @@
 
 # 1. Use a while loop to print numbers 1-10:
-
+b = 0
+while b<11:
+	print(b)
+	b+=1
 # 2. Use a while loop to print the first 10 multiples of 24:
-
+a = 0
+while a<241:
+	print(a)
+	a+=24
 # 3. Use a while loop to find the average of these numbers:
 numbers = [10,42,-2, 900,5,8,39]
+print(len(numbers))
+c = 0
+for i in numbers:
+	c+=i
+	print(c)
+print(c/7)
+
+i= 0
+total = 0
+while i < len(numbers):
+	total+= numbers[i]
+	i+= 1
+print(total)
+print(total/len(numbers))
+
