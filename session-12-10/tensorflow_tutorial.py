f77cc510e98804dbf733036bcb6994e0fc6c2cf1
Isabel <isabel103003@gmail.com>
2020-01-14 17:32:39 -0500

    no idea but before break

--- session-12-10/tensorflow_tutorial.py
+++ session-12-10/tensorflow_tutorial.py
@@ -6,7 +6,6 @@ import warnings
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
-
 # rest of code from tutorial 
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
@@ -30,6 +29,7 @@ for i in range(25):
 plt.show()
 
 #creates a neural network and adds layers to it
+#pooling is to get less parameters and maxpooling is to take the largest value of the pool
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -38,7 +38,7 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 #shows what the model looks like
 model.summary()
-#flatten makes all the data into one thing and then dense makes it a neural network
+#flatten loses the structure and then dense makes it a neural network
 #(first layer with 64 and the second with 10)
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
@@ -55,13 +55,13 @@ history = model.fit(train_images, train_labels, epochs=10,
                     validation_data=(test_images, test_labels))
 
 #evaluating
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
-plt.legend(loc='lower right')
+#plt.plot(history.history['accuracy'], label='accuracy')
+#plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+#plt.xlabel('Epoch')
+#plt.ylabel('Accuracy')
+#plt.ylim([0.5, 1])
+#plt.legend(loc='lower right')
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+#test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
-print(test_acc)
+#print(test_acc)
