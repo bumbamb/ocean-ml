1b19be36851e28abac173afb54cbd7abdc76a3ad
Isabel <isabel103003@gmail.com>
2020-03-24 16:39:03 -0400

    3-24

--- session-3-19/writing-exec-files.py
+++ session-3-19/writing-exec-files.py
@@ -1,1 +1,22 @@
 # to do
+print(#!/bin/csh
+
+
+set dir = ./ss1/
+set fileinfo = {$dir}info_pr.dat
+set filedh =  {$dir}dh000105.gr
+set filest =  {$dir}density000105.gr
+set filestm = {$dir}ss1_st0.dat
+set filequ =  {$dir}ss1a2qu.gr
+set fileqv =  {$dir}ss1a2qv.gr
+set fileqdi = {$dir}ss1a2qdi.gr
+
+./vectorq.exe << !
+'$fileinfo'	#>>>>>Escribe info file info.dat:
+'$filedh'	#>>>>>Escribe fichero de altura Dinamica:
+'$filest'	#>>>>>Escribe fichero de densidad:
+'$filestm'	#>>>>>Escribe fichero de densidad promedio:
+'$filequ'	#>>>>>Escribe fichero Qu:
+'$fileqv'	#>>>>>Escribe fichero Qv:
+'$fileqdi'	#>>>>>Escribe fichero Qdi:
+!)
