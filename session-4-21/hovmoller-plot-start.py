7cc0da5fcf2a2b7e29cc48470b5fb976f43d1362
Isabel <isabel103003@gmail.com>
2020-04-28 16:54:44 -0400

    4-28

--- session-4-21/hovmoller-plot-start.py
+++ session-4-21/hovmoller-plot-start.py
@@ -5,3 +5,20 @@ from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
 
+
+dataset = Dataset(r'/Users/brownscholar/Desktop/fortran_files/omega.nc')
+
+w = dataset['w']
+lat = dataset['latitude']
+title = "hovmoller diagram at latitude " + str(lat[0])
+#depth = 0
+#latitude = 20.125
+
+w_lat = w[:, 0, :, 0]
+w_lat = np.swapaxes(w_lat,0,1)
+print(w_lat.shape)
+cmap = cm.get_cmap('bwr')
+
+plt.pcolormesh(w_lat, cmap = cmap)
+plt.title(title)
+plt.show()
